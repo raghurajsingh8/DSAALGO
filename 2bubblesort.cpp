6a7261f7b8b5ef1a35f8e22e2cabd77d80e69e7e
RAGHURAJ PRATAP SINGH <136258185+raghurajsingh8@users.noreply.github.com>
2024-06-19 19:32:14 +0530

    Update 2bubblesort.cpp
    optimi.. using swapped and break

--- 2bubblesort.cpp
+++ 2bubblesort.cpp
@@ -9,14 +9,19 @@ int main(){
         cin>>arr[k];
     }
     
-    
+    int swapped=0;
     // bubble sort start
     
    for(int i=n-2;i>=0;i--){
+       swapped=0;
        for(int j=0;j<=i;j++){
            if(arr[j]>arr[j+1]){
                swap(arr[j],arr[j+1]);
+               swapped=1;
            }
+       }
+       if(swapped==0){
+           break;
        }
    }
     
